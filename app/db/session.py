9a5cda21397c9119c8e553a3e723d0a3e8858869
HEO KANGSEOK <riverstone@dgu.ac.kr>
2026-05-08 17:26:12 +0900

    Change DATABASE_URL to get_database_url method

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
 engine = create_engine(
-    settings.DATABASE_URL,
+    settings.get_database_url,
     pool_pre_ping=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
